5d20c84f942f66756f8173de2eac7b261404359b
yunchipang <yunchipang@gmail.com>
2023-10-05 18:00:55 -0700

    edit homepage, add github repo links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,35 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <section>
-      <h1>Welcome to MoveMakers🕺🏻</h1>
+    <section style={{ textAlign: "center", margin: "0 auto" }}>
+      <h1 style={{ marginTop: "60px" }}>Welcome to MoveMakers🕺🏻</h1>
+      <ul>
+        <li>
+          <Link
+            href="https://github.com/yunchipang/movemakers-react"
+            target="_blank"
+          >
+            movemakers-react
+          </Link>
+        </li>
+        <li>
+          <Link
+            href="https://github.com/yunchipang/movemakers-fastapi"
+            target="_blank"
+          >
+            movemakers-fastapi
+          </Link>
+        </li>
+        <li>
+          <Link
+            href="https://github.com/yunchipang/movemakers-ios-app"
+            target="_blank"
+          >
+            movemakers-ios-app
+          </Link>
+        </li>
+      </ul>
     </section>
   );
 }
